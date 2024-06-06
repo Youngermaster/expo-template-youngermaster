3b7241518baf54a0037095514ff69f61a6a3de99
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-06-06 17:44:12 -0500

    Refactor Button component to remove unused TouchableOpacityProps import

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import { styles } from "~/styles/globalStyles";
 
-interface ButtonProps extends TouchableOpacityProps {
+interface ButtonProps {
   title: string;
   onPress: () => void;
 }
