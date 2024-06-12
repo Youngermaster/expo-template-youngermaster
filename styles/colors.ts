6ed668dc3f03c471d3e63d643986a20c307ab0bb
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-06-11 22:14:25 -0500

    feat: Add colors module for consistent color usage
    The commit adds a new colors module in the styles folder, which exports an object containing various color values. This change ensures consistent color usage throughout the application by centralizing color definitions in one place.

---
+++ styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#BDBDBD",
+  greenVolt: "#39BF68",
+  blueSky: "#4ABDF1",
+  yellowSun: "#F9C80E",
+  redFire: "#FF4E50",
+  purpleRain: "#8A2387",
+};
+
+export default colors;
